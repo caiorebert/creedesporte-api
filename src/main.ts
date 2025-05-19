a914d36a0e9ab017364b4be9a06d6038573a2e9e
unknown <caiorebert070802.cr@gmail.com>
2025-05-19 08:59:59 -0300

    bloqueando requisição de fora do origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
