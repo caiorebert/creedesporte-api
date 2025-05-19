587a0683fb91218b0fb9ad159545b1713e0b806a
unknown <caiorebert070802.cr@gmail.com>
2025-05-18 21:44:49 -0300

    aumentando limite de requisição

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization',
   });
 
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ limit: '10mb', extended: true }));
+  
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
